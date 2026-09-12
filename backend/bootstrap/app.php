5c34c1f7ec3a49cf1087b4f244be96e3a0ba211f
snydi <d-snyatkov@mail.ru>
2026-09-13 00:27:22 +0300

    fix: Renders problems

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -15,7 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PROTO,
+        );
         $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
